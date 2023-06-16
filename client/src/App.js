89ace32dd675b989cce34fa47a1cb5199f49aca5
Maikelek <maikelekmain@gmail.com>
2023-06-16 23:30:45 +0200

    ⚙️ React Application + React Router

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import {BrowserRouter,Routes,Route,} from "react-router-dom";
+
+import "./App.css";
+
+
+
+function App() {
+  return ( 
+    <div className="App">
+      <BrowserRouter>  
+        <Routes>
+
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
